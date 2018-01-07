1175866c51b030802f388060cb19a0ebc92fb641
mtuchkov <tuchkov.michail@gmail.com>
2018-01-06 17:50:25 -0800

    fix colors

--- src/application/theme.ts
+++ src/application/theme.ts
@@ -21,16 +21,16 @@ export var theme =
             "text_color_primary": "text-primary",
             "text_color_danger": "text-danger",
             "text_color_inverted": "text-inverted",
-            "text_lead": "lead",
-            container: {
-                background: {
-                    "section_bg_default": "section-default",
-                    "section_bg_1": "section-smoke",
-                    "section_bg_2": "section-darker-smoke",
-                    "section_bg_3": "section-dark-smoke",
-                    "section_bg_4": "section-hightlighted"
-                }
-            }
+            "text_lead": "lead"
+        }
+    },
+    container: {
+        background: {
+            "section_bg_default": "section-default",
+            "section_bg_1": "section-smoke",
+            "section_bg_2": "section-darker-smoke",
+            "section_bg_3": "section-dark-smoke",
+            "section_bg_4": "section-hightlighted"
         }
     }
 };
