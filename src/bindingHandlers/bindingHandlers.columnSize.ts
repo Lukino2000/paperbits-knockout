6846966f1215a8785219d47ea8b195b35cc8e9e8
azaslonov <zaslonov.alexander@gmail.com>
2018-02-27 19:59:25 -0800

    more complex row layout selecton. fix around hyperlink pointer events.

--- src/bindingHandlers/bindingHandlers.columnSize.ts
+++ src/bindingHandlers/bindingHandlers.columnSize.ts
@@ -2,7 +2,25 @@
 
 ko.bindingHandlers["columnSize"] = {
     init(element: HTMLElement, valueAccessor) {
-        let size = valueAccessor();
+        let sizes = valueAccessor();
+        let size: number = sizes.xs;
+
+        if (sizes.sm) {
+            size = sizes.sm;
+        }
+
+        if (sizes.md) {
+            size = sizes.md;
+        }
+
+        if (sizes.lg) {
+            size = sizes.lg;
+        }
+
+        if (sizes.xl) {
+            size = sizes.xl;
+        }
+
         let css = {};
 
         css[`col-cfg-${size}`] = true;
