ef19659e5335a7bf93273143847850c4d7c50e83
Igor O <ygrik@hotmail.com>
2018-03-17 10:38:21 -0700

    themes cleanup

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,6 @@ const runSeq = require("run-sequence");
 const webpack = require("webpack");
 const webpackDevServer = require("webpack-dev-server");
 
-
-gulp.task("webpack-dev", (callback) => {
-    var webPackConfig = require("./webpack.config.js");
-    webpack(webPackConfig, function (err, stats) {
-        if (err) throw new gutil.PluginError("webpack", err);
-
-        gutil.log("[webpack-dev]", stats.toString({
-            colors: true,
-            progress: true
-        }));
-
-        callback();
-    });
-});
-
 gulp.task("build-npm", ["build-npm-ts"], () => {
     return gulp.src(["!./src/themes/**/*.*", "./src/**/*.+(html|scss|png|woff|woff2|eot|ttf|svg)"])
         .pipe(gulp.dest("./dist/lib"));
@@ -31,9 +16,7 @@ gulp.task("build-npm", ["build-npm-ts"], () => {
 
 gulp.task("build-npm-ts", ["build-clean"], (callback) => {
     const typescriptProject = typescriptCompiler.createProject("./npm-tsconfig.json", {
-        typescript: typescript,
-        declaration: true,
-        rootDir: "./src"
+        typescript: typescript
     });
 
     const tsResult = typescriptProject
@@ -46,27 +29,11 @@ gulp.task("build-npm-ts", ["build-clean"], (callback) => {
     ]);
 });
 
-gulp.task("server", ["build"], () => {
-    const webPackConfig = require("./webpack.config.js");
-    const options = {
-        host: "0.0.0.0",
-        contentBase: "./dist/client",
-        hot: true
-    };
-    webpackDevServer.addDevServerEntrypoints(webPackConfig, options);
-    const compiler = webpack(webPackConfig);
-    const server = new webpackDevServer(compiler, options);
-    server.listen(8080, "localhost", function (err) {
-        if (err) throw new gutil.PluginError("webpack-dev-server", err);
-        gutil.log("[webpack-dev-server]", "http://localhost:8080/webpack-dev-server/index.html");
-    });
-});
-
 gulp.task("build-clean", (done)  => {
-        return  del(["dist/client/**"], done);
+        return  del(["dist/lib/**"], done);
 });
 
-gulp.task("build", (done) => runSeq("build-clean", "webpack-dev", done));
+gulp.task("build", (done) => runSeq("build-clean", "build-npm-ts", done));
 
-gulp.task("default", ["server"]);
+gulp.task("default", ["build"]);
 
